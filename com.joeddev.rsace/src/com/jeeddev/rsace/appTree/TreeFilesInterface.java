58de5244ce1ecebfdd75535c221f5e27c952205c
joseortizcostadev <joseortizcostadev@gmail.com>
2015-06-04 15:02:26 -0700

    Add javadocs to TreeFilesInterface

--- com.joeddev.rsace/src/com/jeeddev/rsace/appTree/TreeFilesInterface.java
+++ com.joeddev.rsace/src/com/jeeddev/rsace/appTree/TreeFilesInterface.java
@@ -1,5 +1,12 @@
+/**
+ * @author        Jose Ortiz Costa
+ * @application   com.joeddev.rsace
+ * @File          TreeFilesInterface.java
+ * @Date          04/06/2015
+ * @Description   Creates signature methods to be implemented
+ *                by others classes  
+ */
 package com.jeeddev.rsace.appTree;
-
 public interface TreeFilesInterface 
 {
     public void insertData (String filename, Object [] data);
