8b884e983e64fa773b87d925cd2a16630642e7cd
joseortizcostadev <joseortizcostadev@gmail.com>
2015-06-04 09:32:12 -0700

    Add more javadocs

--- com.joeddev.rsace/src/com/jeeddev/rsace/appTree/TreeBuilder.java
+++ com.joeddev.rsace/src/com/jeeddev/rsace/appTree/TreeBuilder.java
@@ -1,5 +1,5 @@
 package com.jeeddev.rsace.appTree;
-// init javadoc
+// init javadoc more docs
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.eclipse.core.resources.IFile;
