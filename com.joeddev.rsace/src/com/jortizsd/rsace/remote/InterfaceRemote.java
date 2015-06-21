dae8a1f06eed0e60c2408bff6bd48b2c576a255e
joseortizcostadev <josedev@jortizsd.com>
2015-06-21 13:53:39 -0700

    Add a temporal rmi test

--- com.joeddev.rsace/src/com/jortizsd/rsace/remote/InterfaceRemote.java
+++ com.joeddev.rsace/src/com/jortizsd/rsace/remote/InterfaceRemote.java
@@ -8,6 +8,7 @@
  */
 package com.jortizsd.rsace.remote;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import org.eclipse.core.resources.IFile;
 
@@ -15,12 +16,13 @@ import com.jortizsd.rsace.team.Developer;
 
 public interface InterfaceRemote extends Remote
 {
-    public Developer getRemoteDeveloper();
-    public void sendRemoteInvitation(String url, Developer dev);
-    public void sendRemoteFile (IFile file);
-    public IFile getRemoteFile();
-    public boolean isServerWorking(String url);
-    public IFile getUpdatedFile ();
-    public void sendUpdatesInFile (IFile file);
-    
+    // public Developer getRemoteDeveloper(String developerId) throws RemoteException;
+   // public void registerRemoteDeveloper (String teamId, String developerId, String developerName, String developerEmail) throws RemoteException;
+   // public boolean doesRemoteDeveloperExist (String developerId) throws RemoteException;
+   // public void updateRemoteDeveloperInfo (String developerId, String tag, String value);
+   // public void deleteRemoteDeveloper (String developerId) throws RemoteException;
+   // public boolean isServerUp (String server) throws RemoteException;
+   // public void addDeveloperToRemoteTeam (String developerId, String teamId) throws RemoteException;
+   // public void createRemoteConfigurationFile () throws RemoteException;
+	public void hello (String name) throws RemoteException;
 }
